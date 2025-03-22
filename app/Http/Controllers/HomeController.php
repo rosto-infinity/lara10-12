adddb8cae314393c5da24e7dfb221da6050f82a0
ROSTAND LELE <rostoinfocus@gmail.com>
2025-03-22 17:07:08 +0100

    Ajout du contrôleur HomeController et mise à jour de la route '/' pour utiliser ce contrôleur

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(Request $request)
+    {
+        // return view('welcome');
+        return [
+            'name' => 'John Doe',
+            'age' => 30,
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
  <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return "view('welcome')";
-});
+Route::get('/', [HomeController::class, 'index']);
 Route::get('/about', action: fn ()=> [
         'name' => 'John Doe',
         'age' => 30,
