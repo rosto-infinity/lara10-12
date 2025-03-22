41e3097aea6fa6f413bbde4168aa417b547706fe
ROSTAND LELE <rostoinfocus@gmail.com>
2025-03-22 16:51:02 +0100

    Ajout de la route '/about' pour retourner des informations sur un utilisateur

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "view('welcome')";
 });
+Route::get('/about', action: fn ()=> [
+        'name' => 'John Doe',
+        'age' => 30,
+    ]); 
