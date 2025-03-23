61484d355f0c83bba14ea12a067fa36b4b9e0a31
ROSTAND LELE <rostoinfocus@gmail.com>
2025-03-23 09:03:36 +0100

    Implement index and show methods in PostController; add routes for posts

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
  <?php
 
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/about', [HomeController::class, 'about']); 
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{id}', [PostController::class, 'show']);
 
 
 
