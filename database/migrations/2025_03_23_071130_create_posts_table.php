8c3ccd9ad7f341f411e44169a39a4ed90ba59ca5
ROSTAND LELE <rostoinfocus@gmail.com>
2025-03-23 08:16:15 +0100

    Add title, slug, and content fields to posts table migration

--- database/migrations/2025_03_23_071130_create_posts_table.php
+++ database/migrations/2025_03_23_071130_create_posts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->longText('content');
             $table->timestamps();
         });
     }
