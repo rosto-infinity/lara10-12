dafe23077a2dce92cae86d36ce454c5251629ef7
ROSTAND LELE <rostoinfocus@gmail.com>
2025-03-25 21:23:30 +0100

    Amélioration de la mise en page de la page d'accueil du blog avec des styles et une structure de contenu améliorés

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,18 +1,15 @@
 @extends('layout')
-  @section('title', 'Accueil Blog-Prof---')
+@section('title', 'Accueil Blog-Prof---')
 @section('content')
-<h1 class="text-4xl font-bold">Mon blog</h1>
-{{-- {{ dump($posts) }} --}}
-  @foreach ($posts as $post)
-    <article>
-      <h2 class="text-green-500 pb-2 pt-7"> {{ $post->title }} </h2>
-      <p class="text-gray-500 pb-2">
-        {{ $post->content }}
-      </p>
-      <b>Créer le</b> <i>{{ $post->created_at }}</i>
-    </article> 
-  @endforeach
-  <div class="text-green-500 pt-7"> 
+<h1 class="text-4xl font-bold text-center mb-6">Mon blog</h1>
+@foreach ($posts as $post)
+  <article class="border p-4 rounded-lg shadow-md mb-4">
+    <h2 class="text-green-500 text-2xl font-semibold pb-2 pt-7">{{ $post->title }}</h2>
+    <p class="text-gray-700 text-base pb-2">{{ $post->content }}</p>
+    <b class="text-gray-600">Créé le :</b> <i class="text-gray-500">{{ $post->created_at->format('d M Y') }}</i>
+  </article>
+@endforeach
+<div class="text-green-500 pt-7">
   {{ $posts->links() }}
- </div>
+</div>
 @endsection
