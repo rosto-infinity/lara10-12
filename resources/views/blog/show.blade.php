c7c91099aaea864adccd6bd78d4d7a73df24dd03
ROSTAND LELE <rostoinfocus@gmail.com>
2025-03-27 19:56:12 +0100

    show-article

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,5 +1,15 @@
 @extends('layout')
 @section('title', 'Page article')
 @section('content')
+<h1 class="text-4xl font-bold text-center mb-6">Mon blog</h1>
+
+  <article class="border p-4 rounded-lg shadow-md mb-4">
+    <h2 class="text-green-500 text-2xl font-semibold pb-2 pt-7">{{ $post->title }}</h2>
+    <p class="text-gray-700 text-base pb-2">{{ $post->content }}</p>
+   
+    
+    </p>
+  </article>
+
 
 @endsection
