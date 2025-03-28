59d66208027ac2adcfda010d5bc4d1b74f7e3ece
ROSTAND LELE <rostoinfocus@gmail.com>
2025-03-28 01:27:01 +0100

    form new article

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -3,7 +3,12 @@
 @extends('layout')
 @section('title', 'Créer article')
 @section('content')
+  <form action="" method="post">
+    <input type='text' name='title' value='Article de demonstration'>
+    <textarea name='content' > Contenu de demonstration' </textarea> 
+    <button>Enregistrer</button>
 
+  </form>
  
 
 @endsection
