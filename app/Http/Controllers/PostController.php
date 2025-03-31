00bfe8008569274c0b8a59c7b4d8de816caa2f6d
ROSTAND LELE <rostoinfocus@gmail.com>
2025-03-31 13:50:19 +0100

    Add unit tests for critical components and features.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -83,9 +83,11 @@ class PostController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Post $post)
     {
-        
+        return view('blog.edit', [
+            'post' => $post,
+        ]);
     }
 
     /**
