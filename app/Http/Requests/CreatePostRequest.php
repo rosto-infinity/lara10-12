3bf9e0e5d52e0dd82c95025f624433514821ffe1
ROSTAND LELE <rostoinfocus@gmail.com>
2025-03-31 11:05:51 +0100

    Mise à jour de CreatePostRequest pour autoriser les requêtes et ajout des règles de validation pour le titre, le contenu et le slug

--- app/Http/Requests/CreatePostRequest.php
+++ app/Http/Requests/CreatePostRequest.php
@@ -11,7 +11,7 @@ class CreatePostRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class CreatePostRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => ['required', 'string', 'max:255'], // Règles pour le titre
+            'content' => ['required', 'string'], // Règles pour le contenu
+            'slug' => ['required', 'string', 'max:255', 'unique:posts,slug'], // Règles pour le slug
         ];
     }
 }
