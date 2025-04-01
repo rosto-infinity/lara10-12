4b6dfd265ed2457b3a9314cb25938593c3e6436f
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-01 18:50:35 +0100

    Update styles to match new design guidelines provided.

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -1,51 +1,5 @@
 @extends('layout')
 @section('title', 'Créer un article article')
 @section('content')
-<div class="max-w-2xl mx-auto bg-white p-6 rounded-lg shadow-md mt-10">
-    <h2 class="text-2xl font-bold text-gray-800 mb-4">Créer un nouvel article</h2>
-    <form action="" method="post" class="space-y-4">
-      @csrf
-        
-        <div>
-            <label for="title" class="block text-sm font-medium text-gray-700">Titre</label>
-            <input type="text" name="title" id="title"  value="{{ old('title') }}"
-            placeholder="Titre de l'article"
-                   class="w-full mt-1 p-2 border border-gray-300 rounded-lg focus:ring focus:ring-green-200 focus:border-green-500">
-                   
-            @error('title')
-                <p class="text-red-500 text-sm mt-1">
-                  {{-- Le champ titre est obligatoire. --}}
-                  {{ $message }}
-                </p>
-            @enderror
-        </div>
-        <div>
-            <label for="slug" class="block text-sm font-medium text-gray-700">Slug</label>
-            <input type="text" name="slug" id="slug" value="{{ old('slug') }}"
-            placeholder="Slug de l'article"
-                   class="w-full mt-1 p-2 border border-gray-300 rounded-lg focus:ring focus:ring-green-200 focus:border-green-500">
-            @error('slug')
-                <p class="text-red-500 text-sm mt-1">
-                  {{-- Le champ slug est obligatoire. --}}
-                  {{ $message }}
-                </p>
-            @enderror
-       
-        <div>
-            <label for="content" class="block text-sm font-medium text-gray-700">Contenu</label>
-            <textarea name="content" id="content" rows="5" 
-            value=""
-            placeholder="Écrivez votre article ici..."
-                      class="w-full mt-1 p-2 border border-gray-300 rounded-lg focus:ring focus:ring-green-200 focus:border-green-500">{{ old('content') }}</textarea>
-            @error('content')
-                <p class="text-red-500 text-sm mt-1">
-                  {{-- Le champ contenu est obligatoire. --}}
-                  {{ $message }}
-                </p>
-            @enderror
-        </div>
-        <button type="submit" 
-                class="w-full bg-green-500 text-white p-2 rounded-lg hover:bg-green-600 transition">Enregistrer</button>
-    </form>
-</div>
+  @include('blog.form')
 @endsection
