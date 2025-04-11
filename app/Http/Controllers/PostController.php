1a8cd2f1e3ff3fc68dddcff046a4474ce5417f97
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-11 20:05:58 +0100

    6-Remove the specified resource from storage

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -113,7 +113,7 @@ class PostController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
+     * 6-Remove the specified resource from storage.
      */
     public function destroy(string $id)
     {
